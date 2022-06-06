2d81e54881f0f6c7e8af60edc9666b5bc4ffac68
JBig_home-note <jbiglion22@google.com>
2022-06-06 10:44:50 +0900

    2022/6/6

--- app/src/main/java/com/jbiglion22/koreanhistorynote/MainActivity.kt
+++ app/src/main/java/com/jbiglion22/koreanhistorynote/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jbiglion22.koreanhistorynote
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -9,8 +10,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.jbiglion22.koreanhistorynote.databinding.ActivityMainBinding
 
 val LOGTAG = "KOREAHISTORYNOTE"
-
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,20 +19,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-
-        binding.rvTitlename.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.rvTitlename.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.rvTitlename.setHasFixedSize(true)
 
         binding.rvTitlename.adapter = TitleNameAdapter(titlenameList)
 
 
+        if (savedInstanceState != null) {
+            // 회전시 초기화되는 내용을 처리하기위해 사용
 
-        val intent = Intent(this, IntroActivity::class.java)
-        intent.putExtra("curPos", 1)
-        startActivity(intent)
-
-
+        } else {
+            // 첫 실행일 경우만 실행
+            val intent = Intent(this, IntroActivity::class.java)
+            intent.putExtra("curPos", 1)
+            startActivity(intent)
+        }
     }
 
 
@@ -52,4 +53,9 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+
+    // 회전시 데이터 저장을 위해
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
 }
